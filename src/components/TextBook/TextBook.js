65edf55cc2df257bd3c1a1666db6d8f2b4782135
Roman Kruglyanskiy <eviltroll@yandex.ru>
2021-03-25 13:26:56 +0300

    refactor: split WordsList into 2 components - TextBookListContainer and WordsList to make it reusable

--- src/components/TextBook/TextBook.js
+++ src/components/TextBook/TextBook.js
@@ -1,16 +1,15 @@
 import React from 'react';
-
 import styles from './TextBook.module.scss';
 import MainTitle from "../MainTitle/MainTitle";
 import UnitsList from "../UnitsList/UnitsList";
-import WordsList from "../WordsList/WordsList";
+import TextBookListContainer from '../TextBookListContainer/TextBookListContainer'
 
 export default function TextBook() {
     return (
         <main className={styles.main}>
             <MainTitle text={"Электронный учебник"} icon={true}/>
             <UnitsList/>
-            <WordsList/>
+            <TextBookListContainer/>
         </main>
     );
 }
--- src/components/WordsList/WordsList.js
+++ src/components/WordsList/WordsList.js
@@ -1,60 +1,10 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {withRouter} from 'react-router-dom';
+import React from 'react';
 import styles from './WordsList.module.scss';
 import WordItem from "../WordItem/WordItem";
-import {fetchWords} from '../../redux/wordsReducer';
 import {baseUrl} from '../../services/baseUrl/baseUrl';
 import {Pagination} from '../Pagination/Pagination';
-import {setCurrentPagesArray, setIsWordButtonsShown, setIsWordTranslated} from '../../redux/appReducer';
-import {setCurrentPagesItem} from '../../redux/appReducer';
 
-
-function WordsList({match}) {
-
-    const dispatch = useDispatch();
-
-    const currentGroup = match.params.unit - 1; // номер текущей группы
-    const currentPage = useSelector(state => state.app.currentPagesArray[currentGroup]); // номер текущей страницы
-
-    const words = useSelector(state => state.words.items);
-    const currentPagesArray = useSelector(state => state.app.currentPagesArray);
-
-    useEffect(() => {
-
-        // Записываем массив текущих страниц из LS в store
-        const lSPagesArray = JSON.parse(localStorage.getItem('currentPagesArray'))
-        lSPagesArray && dispatch(setCurrentPagesArray(lSPagesArray))
-
-        // Записываем настройки букв из LS в store
-        const lSWordSettings = JSON.parse(localStorage.getItem('wordSettings'))
-        lSWordSettings && dispatch(setIsWordTranslated(lSWordSettings.isWordTranslated))
-        lSWordSettings && dispatch(setIsWordButtonsShown(lSWordSettings.isWordButtonsShown))
-
-    }, [dispatch]);
-
-
-    useEffect(() => { // Загружаем слова
-
-        dispatch(fetchWords({group: currentGroup, page: currentPage}));
-
-    }, [dispatch, currentPage, currentGroup]);
-
-    useEffect(() => { // Переписываем массив текущих страниц в LS при его изменении
-
-        localStorage.setItem('currentPagesArray', JSON.stringify(currentPagesArray))
-
-    }, [currentPagesArray]);
-
-
-    const handlePageClick = (e) => { // Обработка нажатия на кнопку пагинации
-
-        dispatch(setCurrentPagesItem({
-            currentGroup: currentGroup,
-            currentPage: e.selected.toString()
-        }));
-
-    };
+export function WordsList({words, handlePageClick, currentPage}) {
 
     const allWords = words.map(({
                                     id,
@@ -100,11 +50,9 @@ function WordsList({match}) {
             <div className={styles.words}>
                 {allWords}
             </div>
-
-
         </>
     );
 }
 
-export default withRouter(WordsList)
+
 
