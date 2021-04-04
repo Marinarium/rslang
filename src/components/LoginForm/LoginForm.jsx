ef0c082db37194ad173ecd890a54b6ed6b53b7e4
Marinarium <schwmk@gmail.com>
2021-04-04 22:15:19 +0300

    feat: add styles for form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {fetchWords, pageFormChange} from '../../redux/wordsReducer'
 import {useDispatch, useSelector} from 'react-redux'
 import {authLogin, loginFormChange} from '../../redux/authReducer'
+import styles from "../RegisterForm/RegisterForm.module.scss";
+import MainTitle from "../MainTitle/MainTitle";
 
 
 export const LoginForm = () => {
@@ -17,14 +19,19 @@ export const LoginForm = () => {
 
     }
     return (
-        <div>
-            Вход:<br/>
-            Enter Email<br/>
-            <input onChange={changeHandler} name='email'/><br/>
-            Enter Password<br/>
-            <input onChange={changeHandler} name='password'/><br/>
-            <button onClick={submitHandler}>Load</button>
-            <br/><br/>
-        </div>
+        <form className={styles.form}>
+            <MainTitle text={'Вход'}/>
+            <div className={styles.field}>
+                <label className={styles.label} htmlFor="mail">Email</label>
+                <input className={styles.input} onChange={changeHandler} name='email' id='mail'/>
+                <div className={styles.line}/>
+            </div>
+            <div className={styles.field}>
+                <label className={styles.label} htmlFor="password">Пароль</label>
+                <input className={styles.input} onChange={changeHandler} name='password' id='password'/>
+                <div className={styles.line}/>
+            </div>
+            <button className={styles.button} onClick={submitHandler}>Войти</button>
+        </form>
     )
 }
