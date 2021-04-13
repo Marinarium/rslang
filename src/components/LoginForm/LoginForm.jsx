26172b10a627c0f33fa473e697a03233956c671f
Kruglyanski <eviltroll@yandex.ru>
2021-04-13 09:07:14 +0300

    feat: add client-side validation

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useHistory} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import {authLogin, loginFormChange} from '../../redux/authReducer';
 import MainTitle from "../MainTitle/MainTitle";
-
+import {validateControl} from '../../services/utils/validation';
 import styles from "../RegisterForm/RegisterForm.module.scss";
 
 export const LoginForm = () => {
@@ -11,8 +11,51 @@ export const LoginForm = () => {
     const dispatch = useDispatch();
     const loginForm = useSelector(state => state.auth.loginForm);
     const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+    const [nameValidation, setNameValidation] = useState({
+        name: 'name',
+        valid: false,
+        touched: false,
+        validation: {
+            required: true,
+            name: true
+        },
+        errorMessage: 'Введите корректное имя'
+    });
+
+    const [emailValidation, setEmailValidation] = useState({
+        name: 'email',
+        valid: false,
+        touched: false,
+        validation: {
+            required: true,
+            email: true
+        },
+        errorMessage: 'Введите корректный email'
+    });
+    const [passwordValidation, setPasswordValidation] = useState({
+        name: 'password',
+        valid: false,
+        touched: false,
+        validation: {
+            required: true,
+            password: true
+        },
+        errorMessage: 'Пароль должен содержать не менее 8 символов'
+    });
+
     const changeHandler = (event) => {
         dispatch(loginFormChange({[event.target.name]: event.target.value}));
+        const setCurrentState = (prevState) => {
+            return {
+                ...prevState,
+                touched: true,
+                valid: validateControl(event.target.value, prevState.validation)
+            }
+        };
+        if (event.target.name === 'email') setEmailValidation(prevState => setCurrentState(prevState));
+        if (event.target.name === 'password') setPasswordValidation(prevState => setCurrentState(prevState));
+
     };
 
     useEffect(() => {
@@ -22,14 +65,13 @@ export const LoginForm = () => {
     const submitHandler = (event) => {
         event.preventDefault();
         dispatch(authLogin(loginForm));
-
-
     };
     return (
         <form className={styles.form} id="registration">
             <MainTitle text={'Вход'}/>
             <div className={styles.field}>
                 <label className={styles.label} htmlFor="mail">Email</label>
+                <span className={styles.error}>{emailValidation.touched && !emailValidation.valid && emailValidation.errorMessage}</span>
                 <input
                     className={styles.input}
                     onChange={changeHandler}
@@ -37,20 +79,22 @@ export const LoginForm = () => {
                     type='email'
                     id='mail'
                     value={loginForm.email}
-                    required
+
                 />
                 <div className={styles.line}/>
+
             </div>
             <div className={styles.field}>
                 <label className={styles.label} htmlFor="password">Пароль</label>
+                <span className={styles.error}>{passwordValidation.touched && !passwordValidation.valid && passwordValidation.errorMessage}</span>
                 <input
+                    type='password'
                     className={styles.input}
                     onChange={changeHandler}
                     name='password'
-                    type='password'
                     id='password'
                     value={loginForm.password}
-                    required
+
                 />
                 <div className={styles.line}/>
             </div>
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,19 +1,61 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {authRegister, registerFormChange, setIsRegistered} from '../../redux/authReducer'
 import {useDispatch, useSelector} from 'react-redux'
 import {useHistory} from 'react-router-dom'
 import MainTitle from "../MainTitle/MainTitle";
 
 import styles from './RegisterForm.module.scss'
+import {validateControl} from "../../services/utils/validation";
 
 export const RegisterForm = () => {
     const history = useHistory();
     const dispatch = useDispatch();
     const registerForm = useSelector(state => state.auth.registerForm)
-    console.log('registerForm', registerForm)
     const isRegistered = useSelector(state => state.auth.isRegistered)
+
+    const [nameValidation, setNameValidation] = useState({
+        name: 'name',
+        valid: false,
+        touched: false,
+        validation: {
+            required: true,
+            name: true
+        },
+        errorMessage: 'Введите корректное имя'
+    });
+
+    const [emailValidation, setEmailValidation] = useState({
+        name: 'email',
+        valid: false,
+        touched: false,
+        validation: {
+            required: true,
+            email: true
+        },
+        errorMessage: 'Введите корректный email'
+    });
+    const [passwordValidation, setPasswordValidation] = useState({
+        name: 'password',
+        valid: false,
+        touched: false,
+        validation: {
+            required: true,
+            password: true
+        },
+        errorMessage: 'Пароль должен содержать не менее 8 символов'
+    });
     const changeHandler = (event) => {
         dispatch(registerFormChange({[event.target.name]: event.target.value}))
+        const setCurrentState = (prevState) => {
+            return {
+                ...prevState,
+                touched: true,
+                valid: validateControl(event.target.value, prevState.validation)
+            }
+        };
+        if (event.target.name === 'email') setEmailValidation(prevState => setCurrentState(prevState));
+        if (event.target.name === 'name') setNameValidation(prevState => setCurrentState(prevState));
+        if (event.target.name === 'password') setPasswordValidation(prevState => setCurrentState(prevState));
     }
     let fileInput = useRef(null);
 
@@ -37,6 +79,7 @@ export const RegisterForm = () => {
             <p className={styles.instruction}>Для того, чтобы воспользоваться всеми функциями приложения - пожалуйста зарегистрируйтесь, потом прейдите на страницу входа, войдите и перезагрузите страницу. Пароль должен содержать не менее 8 символов</p>
             <div className={styles.field}>
                 <label className={styles.label} htmlFor="mail">Email*</label>
+                <span className={styles.error}>{emailValidation.touched && !emailValidation.valid && emailValidation.errorMessage}</span>
                 <input
                     className={styles.input}
                     onChange={changeHandler}
@@ -50,20 +93,20 @@ export const RegisterForm = () => {
             </div>
             <div className={styles.field}>
                 <label className={styles.label} htmlFor="password">Пароль*</label>
+                <span className={styles.error}>{passwordValidation.touched && !passwordValidation.valid && passwordValidation.errorMessage}</span>
                 <input
                     className={styles.input}
                     onChange={changeHandler}
                     value={registerForm.password}
                     type='password'
-                    minLength='8'
                     name='password'
                     id='password'
-                    required
                 />
                 <div className={styles.line}/>
             </div>
             <div className={styles.field}>
                 <label className={styles.label} htmlFor="name">Никнейм*</label>
+                <span className={styles.error}>{nameValidation.touched && !nameValidation.valid && nameValidation.errorMessage}</span>
                 <input
                     className={styles.input}
                     onChange={changeHandler}
@@ -74,7 +117,7 @@ export const RegisterForm = () => {
                 />
                 <div className={styles.line}/>
             </div>
-            <label className={styles.label} htmlFor="img">Аватарка*</label>
+            <label className={styles.label} htmlFor="img">Аватарка</label>
             <input className={styles.avatar}
                 id="img"
                 accept=".jpg, .jpeg, .png"
