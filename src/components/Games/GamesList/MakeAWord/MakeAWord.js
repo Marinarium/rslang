20ccbd38b1abe411099e89bf9edc32180e579900
Kruglyanski <eviltroll@yandex.ru>
2021-04-14 13:52:19 +0300

    fix: some troubles in MakeAWord

--- src/components/Games/GamesList/MakeAWord/MakeAWord.js
+++ src/components/Games/GamesList/MakeAWord/MakeAWord.js
@@ -4,7 +4,7 @@ import {useGameData} from "../../../../hooks/gameDataHook";
 import {textToHtml} from '../../../../helpers.js'
 import {useInterval} from '../../../../helpers.js'
 import styles from "./MakeAWord.module.scss";
-
+import {useHistory} from 'react-router-dom';
 import Letter from "./Letter/Letter";
 import InputWord from "./InputWord/InputWord";
 import ModalFinish from "../../ModalFinish/ModalFinish";
@@ -13,202 +13,217 @@ import Loader from "../../Loader/Loader";
 import ExitBtn from "../../ExitBtn/ExitBtn";
 
 export default function MakeAWord() {
-  const [arrWords, setArrWords] = useState([]);
-  const [answer, setAnswer] = useState('');
-  const [gameWord, setGameWord] = useState([]);
-  const [correctLettersArr, setCorrectLettersArr] = useState([]);
-  const [gameWordTranslate, setGameWordTranslate] = useState('');
-  const [gameWordDescription, setGameWordDescription] = useState('');
-  const [gameWordTranscription, setGameWordTranscription] = useState('');
-  const [wordComplete, setWordComplete] = useState(false);
-  const [wordLoose, setWordLoose] = useState(false);
-  const [gameActive, setGameActive] = useState(false);
-  const [totalDone, setTotalDone] = useState(false);
-  const [modalActive, setModalActive] = useState(false);
-  const [trueCount, setTrueCount] = useState(0);
-  const [seconds, setSeconds] = useState(5);
+    const [arrWords, setArrWords] = useState([]);
+    const history = useHistory();
+    const [answer, setAnswer] = useState('');
+    const [gameWord, setGameWord] = useState([]);
+    const [correctLettersArr, setCorrectLettersArr] = useState([]);
+    const [gameWordTranslate, setGameWordTranslate] = useState('');
+    const [gameWordDescription, setGameWordDescription] = useState('');
+    const [gameWordTranscription, setGameWordTranscription] = useState('');
+    const [wordComplete, setWordComplete] = useState(false);
+    const [wordLoose, setWordLoose] = useState(false);
+    const [gameActive, setGameActive] = useState(false);
+    const [totalDone, setTotalDone] = useState(false);
+    const [modalActive, setModalActive] = useState(false);
+    const [trueCount, setTrueCount] = useState(0);
+    const [seconds, setSeconds] = useState(5);
+    const [currentWord, setCurrentWord] = useState('');
+    const { goodCount, badCount } = useGameData();
+    const words = useSelector((state) => state.words.items);
+    const userId = useSelector((state) => state.auth.userId);
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+    const token = useSelector(state => state.auth.token);
+    const [currentWordId, setCurrentWordId] = useState('');
+    const [randomWords, setRandomWords] = useState([]);
+    const [activeWord, setActiveWord] = useState(0);
 
-  const { goodCount, badCount } = useGameData();
-  const words = useSelector((state) => state.words.items);
-  const userId = useSelector((state) => state.auth.userId);
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-  const token = useSelector(state => state.auth.token);
-  const [currentWordId, setCurrentWordId] = useState('');
-  const [randomWords, setRandomWords] = useState([]);
-  const [activeWord, setActiveWord] = useState(0);
+    useInterval(() => {
+        if (seconds > 0) {
+            setSeconds((prev) => prev - 1);
+            startGame();
+        }
+    }, 1000);
 
-  useInterval(() => {
-    if (seconds > 0) {
-      setSeconds((prev) => prev - 1);
-      startGame();
+    useEffect(() => {
+        setArrWords(() => [...words])
+    }, [words]);
+
+    useEffect(() => {
+        const activeWordObj = words.find(i => i.word === currentWord.word);
+        activeWordObj && setCurrentWordId(activeWordObj.id)
+    }, [words, activeWord, currentWord]);
+
+    useEffect(() => {
+        setRandomWords(() => arrWords.sort(() => 0.5 - Math.random()));
+        // setRandomWords(() => arrWords);
+    }, [arrWords]);
+
+    function startGame() {
+
+        setGameActive(() => true);
+        setWordComplete(() => false);
+        setWordLoose(() => false);
+        setModalActive(() => false);
+        setActiveWord(0);
+        setTrueCount(0);
+        if (randomWords[0]){
+            setCurrentWord(randomWords[0]);
+            shuffleLetters(randomWords[0].word);
+            getLettersArr(randomWords[0].word);
+            getTranslate(randomWords[0].wordTranslate);
+            console.log(wordLoose, 'wordLoose')
+        } else{
+            history.push('/games')
+        }
     }
-  }, 1000);
 
-  useEffect(() => {
-    setArrWords(() => [...words])
-  }, [words]);
-
-  useEffect(() => {
-    const activeWordObj = words.find(i => i.word === Object.keys(randomWords)[activeWord]);
-    activeWordObj && setCurrentWordId(activeWordObj.id)
-  }, [words, activeWord, randomWords]);
-
-  useEffect(() => {
-    setRandomWords(() => arrWords.sort(() => 0.5 - Math.random()));
-    // setRandomWords(() => arrWords);
-  }, [arrWords]);
-
-  function startGame() {
-    setGameActive(() => true);
-    setWordComplete(() => false);
-    setWordLoose(() => false);
-    setModalActive(() => false);
-    setActiveWord(0);
-    setTrueCount(0);
-    shuffleLetters(randomWords[0].word);
-    getLettersArr(randomWords[0].word);
-    getTranslate(randomWords[0].wordTranslate);
-    console.log(wordLoose, 'wordLoose')
-  }
-
-  function shuffleLetters(word) {
-    word = word.toLowerCase().split('');
-    let j, temp;
-    for (let i = word.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      temp = word[j];
-      word[j] = word[i];
-      word[i] = temp;
+    function shuffleLetters(word) {
+        word = word.toLowerCase().split('');
+        let j, temp;
+        for (let i = word.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = word[j];
+            word[j] = word[i];
+            word[i] = temp;
+        }
+        return setGameWord(() => word)
     }
-    return setGameWord(() => word)
-  }
 
-  function shrinkArr() {
-    if (randomWords.length === 1) {
-      setWordComplete(() => false);
-      setGameActive(() => false);
+    function shrinkArr() {
+
+        if (randomWords.length === 0) {
+            setWordComplete(() => false);
+            setGameActive(() => false);
+        }
+        setAnswer(() => '');
+
+        //return setRandomWords(() => randomWords.slice(1));
     }
-    setAnswer(() => '');
-    return setRandomWords(() => randomWords.slice(1));
-  }
 
-  function getLettersArr(word) {
-    return setCorrectLettersArr((prev) => word.toLowerCase().split(''))
-  }
-
-  function concatenate(letter) {
-    return setAnswer((prev) => prev + letter)
-  }
-
-  function getTranslate(word) {
-    return setGameWordTranslate(() => word);
-  }
-
-  function getDescription(word) {
-    return setGameWordDescription(() => word);
-  }
-
-  function getTranscription(word) {
-    return setGameWordTranscription(() => word);
-  }
-
-  function toLoose() {
-    isAuthenticated && badCount(userId, currentWordId, words, token);
-    setWordComplete(() => true);
-    setWordLoose(() => true);
-    setAnswer(() => randomWords[0].word);
-    getDescription(randomWords[0].textMeaning);
-    getTranscription(randomWords[0].transcription)
-  }
-
-  function nextBtn() {
-    shrinkArr();
-    if (randomWords.length === 0) {
-      setModalActive(() => true);
-      return;
+    function getLettersArr(word) {
+        return setCorrectLettersArr((prev) => word.toLowerCase().split(''))
     }
-    setTotalDone(() => true);
-    setWordComplete(() => false);
-    setWordLoose(() => false);
-    shuffleLetters(randomWords[0].word);
-    getLettersArr(randomWords[0].word);
-    getTranslate(randomWords[0].wordTranslate);
-  }
 
-  function endWord() {
-    setTotalDone(() => true);
-    getDescription(randomWords[0].textMeaning);
-    getTranscription(randomWords[0].transcription)
-  }
+    function concatenate(letter) {
+        return setAnswer((prev) => prev + letter)
+    }
 
-  return (
-    <section className={styles.make_word}>
-      {seconds === 0 ? (
-        <>
-          <ExitBtn />
+    function getTranslate(word) {
+        return setGameWordTranslate(() => word);
+    }
 
-          <h3>Собери слово <span>{gameWordTranslate ? gameWordTranslate : null}</span> из букв</h3>
+    function getDescription(word) {
+        return setGameWordDescription(() => word);
+    }
 
-          {gameActive ? (
-            <main className={styles.make_word_game}>
+    function getTranscription(word) {
+        return setGameWordTranscription(() => word);
+    }
 
-              <InputWord answer={answer}/>
+    function toLoose() {
+        isAuthenticated && badCount(userId, currentWordId, words, token);
+        setWordComplete(() => true);
+        setWordLoose(() => true);
+        setAnswer(() => randomWords[0].word);
+        getDescription(randomWords[0].textMeaning);
+        getTranscription(randomWords[0].transcription)
+    }
 
-              {wordComplete ? (
-                <div>
-                  <div>Transcription: {gameWordTranscription}</div>
-                  <div>Meaning: {textToHtml(gameWordDescription)}</div>
-                </div>
-              ) : (
-                <div className={styles.word_letters}>
-                  {gameWord.map((letter, index) => (
-                    <Letter
-                      key={index}
-                      letter={letter}
-                      correctLettersArr={correctLettersArr}
-                      setCorrectLettersArr={setCorrectLettersArr}
-                      concatenate={concatenate}
-                      endWord={endWord}
-                      totalDone={totalDone}
-                      setTotalDone={setTotalDone}
-                      setWordComplete={setWordComplete}
-                      setWordLoose={setWordLoose}
-                      setTrueCount={setTrueCount}
-                      isAuthenticated={isAuthenticated}
-                      goodCount={goodCount}
-                      userId={userId}
-                      currentWordId={currentWordId}
-                      words={words}
-                      token={token}
-                    />
-                  ))}
-                </div>
-              )}
+    function nextBtn() {
+        if (randomWords.length === 0) {
+            setModalActive(() => true);
+            return;
+        }
+        shrinkArr();
+        setTotalDone(() => true);
+        setWordComplete(() => false);
+        setWordLoose(() => false);
+        if (randomWords[0]){
+            setCurrentWord(randomWords[0]);
+            shuffleLetters(randomWords[0].word);
+            getLettersArr(randomWords[0].word);
+            getTranslate(randomWords[0].wordTranslate);
+        } else {
+            history.push('/games')
+        }
+    }
 
-              <div className={styles.buttons_footer}>
-                <button onClick={toLoose} disabled={wordLoose ? 'disabled' : null}>
-                  Сдаться
-                </button>
-                <button onClick={nextBtn} disabled={wordLoose ? null : 'disabled'}>
-                  Дальше
-                </button>
-              </div>
+    function endWord() {
+        setTotalDone(() => true);
+        getDescription(randomWords[0].textMeaning);
+        getTranscription(randomWords[0].transcription);
+        setRandomWords(() => randomWords.slice(1));
+    }
 
-            </main>
-          ) : null}
+    return (
+        <section className={styles.make_word}>
+            {seconds === 0 ? (
+                <>
+                    <ExitBtn />
 
-          <Modal modalActive={modalActive} setModalActive={setModalActive}>
-            <ModalFinish
-              trueCount={trueCount}
-              looseCount={arrWords.length - trueCount}
-              startGame={startGame}
-            />
-          </Modal>
+                    <h3>Собери слово <span>{gameWordTranslate ? gameWordTranslate : null}</span> из букв</h3>
 
-        </>
-      ) : (
-        <Loader seconds={seconds}/>
-      )}
-    </section>
-  )
+                    {gameActive ? (
+                        <main className={styles.make_word_game}>
+
+                            <InputWord answer={answer}/>
+
+                            {wordComplete ? (
+                                <div>
+                                    <div>Transcription: {gameWordTranscription}</div>
+                                    <div>Meaning: {textToHtml(gameWordDescription)}</div>
+                                </div>
+                            ) : (
+                                <div className={styles.word_letters}>
+                                    {gameWord.map((letter, index) => (
+                                        <Letter
+                                            key={index}
+                                            letter={letter}
+                                            correctLettersArr={correctLettersArr}
+                                            setCorrectLettersArr={setCorrectLettersArr}
+                                            concatenate={concatenate}
+                                            endWord={endWord}
+                                            totalDone={totalDone}
+                                            setTotalDone={setTotalDone}
+                                            setWordComplete={setWordComplete}
+                                            setWordLoose={setWordLoose}
+                                            setTrueCount={setTrueCount}
+                                            isAuthenticated={isAuthenticated}
+                                            goodCount={goodCount}
+                                            userId={userId}
+                                            currentWordId={currentWordId}
+                                            words={words}
+                                            token={token}
+                                        />
+                                    ))}
+                                </div>
+                            )}
+
+                            <div className={styles.buttons_footer}>
+                                <button onClick={toLoose} disabled={wordLoose ? 'disabled' : null}>
+                                    Сдаться
+                                </button>
+                                <button onClick={nextBtn} disabled={wordLoose ? null : 'disabled'}>
+                                    Дальше
+                                </button>
+                            </div>
+
+                        </main>
+                    ) : null}
+
+                    <Modal modalActive={modalActive} setModalActive={setModalActive}>
+                        <ModalFinish
+                            trueCount={trueCount}
+                            looseCount={arrWords.length - trueCount}
+                            startGame={startGame}
+                        />
+                    </Modal>
+
+                </>
+            ) : (
+                <Loader seconds={seconds}/>
+            )}
+        </section>
+    )
 };
