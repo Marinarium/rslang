f30bf63c4fa6139735a69c12ae9a29c396efbcae
Marinarium <schwmk@gmail.com>
2021-04-16 21:14:03 +0300

    fix: fix styles for audiocall

--- src/components/Games/GamesList/Audiocall/Info/Info.js
+++ src/components/Games/GamesList/Audiocall/Info/Info.js
@@ -7,13 +7,13 @@ export default function Info({soundOn, currentAnswer, wordComplete}) {
 
   return (
     <section className={styles.info}>
-      <div>
+      <div className={styles.word}>
         <i onClick={soundOn} className="fas fa-volume-up" style={{color: 'white'}}/>
         <span> {wordComplete ? currentAnswer[4] : null}</span>
       </div>
-      <div>
-        <div>{wordComplete ? (<><span>Транскрипция</span>: {currentAnswer[5]}</>) : null}</div>
-        <div>{wordComplete ? (<><span>Обозначение</span>: {textToHtml(currentAnswer[6])}</>) : null}</div>
+      <div className={styles.about}>
+        <div className={styles.transcription}>{wordComplete ? (<><span>Транскрипция</span>: {currentAnswer[5]}</>) : null}</div>
+        <div className={styles.meaning}>{wordComplete ? (<><span>Обозначение</span>: {textToHtml(currentAnswer[6])}</>) : null}</div>
       </div>
     </section>
   )
